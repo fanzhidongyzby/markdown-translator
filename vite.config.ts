f0be92eb3009c634dea1be0675b5f3a394a4a56c
Florian <florianfan@apache.org>
2025-11-27 13:33:17 +0800

    fix: deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      preview: {
+        allowedHosts: true
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: true
       },
       plugins: [react()],
       define: {
